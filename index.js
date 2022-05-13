46114f74554944b495f93f2261dd3a7c7fae0967
ireland <deannaireland@icloud.com>
2022-05-13 13:33:28 -0400

    Add 404

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Needed Modules
+require('dotenv').config()
 const express = require('express')
 
 // Initialize
@@ -9,7 +10,12 @@ app.get('/', function (req,res) {
     res.send('Hello World')
 })
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
 // Listen
-app.listen(3000, function(){
+app.listen(process.env.PORT, function(){
     console.log('I am awake!')
 })
